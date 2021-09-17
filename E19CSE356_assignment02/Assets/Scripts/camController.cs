1701e6c0b2c25d3958edbc05cd19c2b63768d132
kartikaysharma01 <sharma.kartik2107@gmail.com>
2021-09-17 23:01:59 +0530

    move animal and throw food

--- E19CSE356_assignment02/Assets/Scripts/camController.cs
+++ E19CSE356_assignment02/Assets/Scripts/camController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cameraController : MonoBehaviour
+public class camController : MonoBehaviour
 {
     public GameObject player;   //Public variable to store a reference to the player game object
     private Vector3 offset;     //Private variable to store the offset distance between the player and camera
--- E19CSE356_assignment02/Assets/Scripts/player.cs
+++ E19CSE356_assignment02/Assets/Scripts/player.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed = 10.0f;
+    public float speed = 15.0f;
+    public GameObject foodPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +23,15 @@ public class player : MonoBehaviour
         } else if (transform.position.z > 30) {
             transform.position = new Vector3(transform.position.x, transform.position.y, 30);
         }
+
+        // read keyboard movements for player
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            // Launch a projectie from the user 
+            Instantiate(foodPrefab, transform.position, foodPrefab.transform.rotation);
+        }
     }
 }
