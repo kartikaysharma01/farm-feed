ea8e0eadf147e4c339a41dbe7094e346baeb157b
kartikaysharma01 <sharma.kartik2107@gmail.com>
2021-09-18 16:45:48 +0530

    random spawn animals and add box collider properties to prefab

--- E19CSE356_assignment02/Assets/Scripts/spawnManager.cs
+++ E19CSE356_assignment02/Assets/Scripts/spawnManager.cs
@@ -5,20 +5,27 @@ using UnityEngine;
 public class spawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    private float startDelay = 4;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S)) {
-            Vector3 spawnPos = new Vector3(Random.Range(-22,-10), 0, Random.Range(-10, 30));
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }        
+        // // press S and spawn animals
+        // if (Input.GetKeyDown(KeyCode.S)) {
+        //     SpawnRandomAnimal();
+        // }        
+    }
+
+    void SpawnRandomAnimal() {
+        Vector3 spawnPos = new Vector3(Random.Range(-22,-10), 0, Random.Range(-10, 30));
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
